4db13dbb22793762b8c6a2fc602e3da0a9f6032f
Joshua <joshua.baeuml@smail.inf.ih-brs.de>
2020-10-06 16:50:57 +0200

    added UtilitieTests and changed UML Class Diagram

---
+++ src/test/utilities/requestUtilitiesTest.java
@@ -0,0 +1,33 @@
+package utilities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class requestUtilitiesTest {
+
+    @BeforeEach
+    void init(){
+
+    }
+
+    @Test
+    void getRequestParams() {
+    }
+
+    @Test
+    void getContextParams() {
+    }
+
+    @Test
+    void setContextParam() {
+    }
+
+    @Test
+    void createJson() {
+    }
+
+    @Test
+    void checkAttributes() {
+    }
+}
---
+++ src/test/utilities/responseUtilitiesTest.java
@@ -0,0 +1,63 @@
+package utilities;
+
+import interfaces.VehicleIF;
+import models.Car;
+import models.CarPark;
+import models.Config;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.json.JsonObject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static utilities.responseUtilities.createVehicleAsJson;
+
+
+class responseUtilitiesTest {
+
+    CarPark ph;
+    VehicleIF car;
+    JsonObject v;
+
+    @BeforeEach
+    void init(){
+        ph=new CarPark(new Config());
+        car=new Car(CustomerType.LOCAL,"K-AB-1234");
+        ph.enter(car);
+        car=ph.leaveRandom();
+        v= createVehicleAsJson(car);
+    }
+
+    @Test
+    void createVehicleAsJson_CustomerType(){
+        assertTrue(v.getString("CustomerType")=="LOCAL");
+    }
+
+    @Test
+    void createVehicleAsJson_Licenseplate() {
+        assertEquals(car.getLicensePlate(),v.getString("LicensePlate"));
+    }
+
+    @Test
+    void createVehicleAsJson_EntryDate(){
+        assertEquals(car.getTicket().getEntranceDate().toString(),v.getString("EntryDate"));
+    }
+
+    @Test
+    void createVehicleAsJson_ExitDate(){
+        assertEquals(car.getTicket().getExitDate().toString(),v.getString("ExitDate"));
+    }
+
+    @Test
+    void createVehicleAsJson_Price(){
+        assertEquals(car.getTicket().getPrice(),v.getInt("Price"));
+    }
+
+    @Test
+    void createVehicleAsJson_TicketID(){
+        assertEquals(car.getTicket().getTicketID(),v.getInt("TicketID"));
+    }
+
+
+}
