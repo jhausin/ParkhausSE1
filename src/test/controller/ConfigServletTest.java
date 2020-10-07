5f17f5c716f5c04d3119c817f9a68bc143ae97f2
Joshua Baeuml <joshua.baeuml@smail.inf.h-brs.de>
2020-10-07 18:51:07 +0200

    added my name in some diagrams

---
+++ src/test/controller/ConfigServletTest.java
@@ -0,0 +1,23 @@
+
+/**
+ *Author: Joshua Bäuml
+ */
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigServletTest {
+
+    @Test
+    void doPost() {
+
+    }
+
+
+}
--- src/test/utilities/requestUtilitiesTest.java
+++ src/test/utilities/requestUtilitiesTest.java
@@ -1,18 +1,32 @@
+/**
+ *Author: Joshua Bäuml
+ */
 package utilities;
 
+import utilities.requestUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static utilities.requestUtilities.createJson;
 
 
 class requestUtilitiesTest {
 
+    HttpServletRequest req;
+
     @BeforeEach
     void init(){
-
+        req=createMock(HttpServletRequest.class);
     }
 
     @Test
-    void getRequestParams() {
+    void getRequestParams_() {
+
     }
 
     @Test
@@ -24,7 +38,10 @@ class requestUtilitiesTest {
     }
 
     @Test
-    void createJson() {
+    void createJson_() {
+        expect(req.getParameter("name")).andReturn("Parkhaus").times(1);
+        String s=createJson(req).toString();
+        assertEquals("name: Parkhaus",s);
     }
 
     @Test
